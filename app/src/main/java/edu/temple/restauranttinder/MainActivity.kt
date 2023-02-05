ba60a2315d6890ed9ad6eb063a13282433e1e90b
Harrison Fedor <fedor@temple.edu>
2023-02-05 10:38:26 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/restauranttinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.restauranttinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
